0338d3a768e47bea95d67cda9de2d0c0dfe77853
Lindsey Liu <jmjemi327@gmail.com>
2016-02-18 18:44:57 -0600

    Initial Commit

---
+++ Scavenger HuntTests/Scavenger_HuntTests.swift
@@ -0,0 +1,36 @@
+//
+//  Scavenger_HuntTests.swift
+//  Scavenger HuntTests
+//
+//  Created by Lindsey Liu😉 on 16-02-18.
+//  Copyright (c) 2016年 Lingzi Liu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Scavenger_HuntTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
